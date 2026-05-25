ae7de460e931e036a309add1c37e73755975f94d
Crystian <crystian-aguiar1@hotmail.com>
2026-05-25 00:03:14 -0300

    FrontEnd: teste-front/avalia-ai

--- avalia-ai/src/routes/auth.routes.tsx
+++ avalia-ai/src/routes/auth.routes.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// Importe todas as telas juntas
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import Favorites from '../screens/Favorites';
 import Login from '../screens/Login';
-import Register from '../screens/Register'; // 👉 Tem que importar aqui
+import Register from '../screens/Register';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function AuthRoutes() {
+export default function AppRoutes() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register} />
-    </Stack.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      {/* Coloque o Login como a primeira tela para ele abrir primeiro */}
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Register" component={Register} />
+    </Tab.Navigator>
   );
 }
